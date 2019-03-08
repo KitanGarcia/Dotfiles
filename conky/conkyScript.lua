ba9d66ee83891745e4140bcee8cfe0480d6c14f3
Kitan Garcia <kitangar@gmail.com>
2019-03-07 18:59:20 -0800

    backup conky before messing with lua

--- conky/conkyScript.lua
+++ conky/conkyScript.lua
@@ -11,6 +11,40 @@ function conky_main()
                                          conky_window.width,
                                          conky_window.height)
     cr = cairo_create(cs)
+
+
+
+    font="Mono"
+    font_size=12
+    text="Hello, Kitan"
+    xpos,ypos=50,100
+    red,green,blue,alpha=1,1,1,1
+    font_slant=CAIRO_FONT_SLANT_NORMAL
+    font_face=CAIRO_FONT_WEIGHT_NORMAL
+    ----------------------------------
+    cairo_select_font_face (cr, font, font_slant, font_face);
+    cairo_set_font_size (cr, font_size)
+    cairo_set_source_rgba (cr,red,green,blue,alpha)
+    cairo_move_to (cr,xpos,ypos)
+    cairo_show_text (cr,text)
+    cairo_stroke (cr)
+
+    --TO DRAW A LINE--
+    line_cap=CAIRO_LINE_CAP_BUTT
+    red,green,blue,alpha=1,1,1,1
+    startx=380
+    starty=290
+    line_width=400
+    cairo_set_line_cap (cr, line_cap)
+    cairo_set_source_rgba (cr, red,green,blue,alpha)
+    cairo_move_to (cr, startx, starty)
+    cairo_rel_line_to (cr, 300, -520)
+    --cairo_line_to (cr, 300,200) same as line above--
+    cairo_stroke (cr)
+    ----------------------------------
+
+
+
     local updates=tonumber(conky_parse('${updates}'))
     if updates>5 then
         print ("hello world")
